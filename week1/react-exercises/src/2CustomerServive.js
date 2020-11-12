101eb2f15062d27a0836f41bbe1c89ed20c75397
avamoradi <60700386+avamoradi@users.noreply.github.com>
2020-11-12 10:33:10 +0100

    Update 2CustomerServive.js

--- week1/react-exercises/src/2CustomerServive.js
+++ week1/react-exercises/src/2CustomerServive.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 function Guarantee(props) {
-  return (
+  const service = props.serviceArr.map((item) => 
     <div>
-        <img src={props.imgUrl}
-        alt = {props.title}/>
-        <div> {props.title} </div>
-        <div>{props.description} </div>
+      <img src={item.imgUrl}
+      alt = {item.title}/>
+      <div> {item.title} </div>
+      <div>{item.description} </div>
     </div>
   );
+  return (
+      <div className="flex">{service}</div>
+    );
 }
 
 export default Guarantee;
